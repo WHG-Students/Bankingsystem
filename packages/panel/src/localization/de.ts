0e3b1534662af22eaddd80883b328f87de7f2790
Keimeno <constantin@metzworld.com>
2020-07-10 16:22:17 +0200

    added auth translation keys

--- packages/panel/src/localization/de.ts
+++ packages/panel/src/localization/de.ts
@@ -1,1 +1,58 @@
-export const de = {};
+export const de = {
+  auth: {
+    register: {
+      registerAnAccount: 'Erstelle einen Account',
+      alreadyHaveAnAccount: 'Hast du bereits einen Account?',
+      login: 'Einloggen',
+      register: 'Registrieren',
+      personalQuestions: 'Persönliches',
+      firstName: 'Vorname',
+      lastName: 'Nachname',
+      email: 'Email',
+      age: 'Alter',
+      address: 'Addresse',
+      passwordQuestions: 'Wähle ein Passwort',
+      password: 'Passwort',
+      repeatPassword: 'Gebe dein Passwort erneut ein',
+      addressQuestions: 'Gebe dein Addresse ein',
+      country: 'Land',
+      state: 'Bundesland',
+      city: 'Stadt',
+      zipCode: 'Plz.',
+      street: 'Straße',
+      house: 'Haus Nr.',
+      notice:
+        'Hiermit bestätigen Sie, dass Sie mit unseren Bedingungen einverstanden sind. ' +
+        'Wir tragen keine Haft für die Sicherheit Ihrer Daten. ' +
+        '<b>Bitte geben Sie keine echten Daten an</b>, ' +
+        'da es sich hierbei nur um ein Demonstratives Projekt handelt.',
+      response: {
+        notChecked:
+          'Du musst unseren Bedingungen zustimmen um einen Account erstellen zu können!',
+        accountCreatedSuccessfully:
+          'Dein Account wurde erfolgreich erstellt! Du wirst jetzt weitergeleitet.',
+        conflict: 'Ein Account mit dieser Email Addresse existiert bereits.',
+        internalServerError:
+          'Ein unbekannter Serverfehler ist aufgetreten. Bitte versuche es später erneut.',
+        error: 'Ein unbekannter Fehler ist aufgetreten.',
+      },
+    },
+    login: {
+      loginAccount: 'Logge dich in deinen Account ein',
+      credentials: 'Account',
+      email: 'Email',
+      password: 'Passwort',
+      login: 'Einloggen',
+      register: 'Registrieren',
+      dontHaveAnAccount: 'Hast du noch keinen Account?',
+      response: {
+        unauthorized: 'Dein angegebenes Passwort ist falsch.',
+        notFound:
+          'Es konnte kein Nutzer unter dieser Email Addresse gefunden werden.',
+        internalServerError:
+          'Ein unbekannter Serverfehler ist aufgetreten. Bitte versuche es später erneut.',
+        error: 'Ein unbekannter Fehler ist aufgetreten.',
+      },
+    },
+  },
+};
