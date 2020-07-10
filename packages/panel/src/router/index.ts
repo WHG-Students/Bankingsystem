a4086278a09760fcdde7ff4a806a0a8ce4f8846d
Keimeno <constantin@metzworld.com>
2020-07-10 16:22:56 +0200

    registered auth routes

--- packages/panel/src/router/index.ts
+++ packages/panel/src/router/index.ts
@@ -1,14 +1,38 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
 import Home from '../views/Home.vue';
+import Register from '../views/auth/Register.vue';
+import Login from '../views/auth/Login.vue';
 
 Vue.use(VueRouter);
+
+const component = Vue.extend({
+  render: c => c('router-view'),
+});
 
 const routes: RouteConfig[] = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    redirect: '/auth/register',
+    component,
+    children: [
+      {
+        path: 'register',
+        name: 'Register',
+        component: Register,
+      },
+      {
+        path: 'login',
+        name: 'Login',
+        component: Login,
+      },
+    ],
   },
 ];
 
