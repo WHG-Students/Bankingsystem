3d70c4a2b3b31bf01b971833ea6899013cf235b7
Keimeno <constantin@metzworld.com>
2020-07-10 16:22:37 +0200

    added function to update access token dynamically

--- packages/panel/src/plugins/axios.ts
+++ packages/panel/src/plugins/axios.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import {Key} from '@/store/auth';
+
+const retrieveAccessToken = () => {
+  return localStorage.getItem(Key.ACCESS_TOKEN);
+};
 
 export const api = axios.create({
   baseURL: process.env.VUE_APP_ROOT_API,
+  headers: {
+    Authorization: `Bearer ${retrieveAccessToken()}`,
+  },
 });
+
+export const updateAccessToken = () => {
+  api.defaults.headers = {
+    Authorization: `Bearer ${retrieveAccessToken()}`,
+  };
+};
