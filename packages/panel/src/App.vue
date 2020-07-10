bb04412a0eed867f52416b4c03bbea7f4d2a33f8
Keimeno <constantin@metzworld.com>
2020-07-10 16:20:56 +0200

    added startup handling to redirect to auth routes if not authenticated

--- packages/panel/src/App.vue
+++ packages/panel/src/App.vue
@@ -6,7 +6,34 @@
 
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({});
+import {mapActions, mapGetters} from 'vuex';
+import {Auth} from './store/auth';
+import {logger} from './plugins/winston';
+
+export default Vue.extend({
+  methods: {
+    ...mapActions('auth', [Auth.CHECK_IS_AUTHENTICATED, Auth.DECODE_ID_TOKEN]),
+    ...mapGetters('auth', [Auth.GET_IS_AUTHENTICATED, Auth.GET_USER]),
+  },
+  created() {
+    this[Auth.CHECK_IS_AUTHENTICATED]();
+
+    if (this[Auth.GET_IS_AUTHENTICATED]()) {
+      logger.info('Entry/created: is authenticated');
+      this[Auth.DECODE_ID_TOKEN]();
+      // stay at route, otherwise we would redirect the user
+      // to the overview page after every reload
+    } else {
+      logger.info('Entry/created: is NOT authenticated');
+      const route = this.$route.name;
+
+      // don't redirect if already in auth routes
+      if (route !== 'Register' && route !== 'Login') {
+        this.$router.push({name: 'Auth'});
+      }
+    }
+  },
+});
 </script>
 
 <style lang="scss">
