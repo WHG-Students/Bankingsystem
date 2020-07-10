8ca622fb6cc8ad4e5e3c8c26ac76908ae49fa73f
Keimeno <constantin@metzworld.com>
2020-07-11 01:04:30 +0200

    turned idAuthenticated bool into computed + implemented layout

--- packages/panel/src/App.vue
+++ packages/panel/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <router-view />
+    <layout-header v-show="isAuthenticated" />
+    <router-view class="mb-5" />
   </div>
 </template>
 
@@ -9,18 +10,33 @@ import Vue from 'vue';
 import {mapActions, mapGetters} from 'vuex';
 import {Auth} from './store/auth';
 import {logger} from './plugins/winston';
+import LayoutHeader from './components/layout/Header.vue';
 
 export default Vue.extend({
+  components: {
+    LayoutHeader,
+  },
   methods: {
     ...mapActions('auth', [Auth.CHECK_IS_AUTHENTICATED, Auth.DECODE_ID_TOKEN]),
     ...mapGetters('auth', [Auth.GET_IS_AUTHENTICATED, Auth.GET_USER]),
   },
-  created() {
+  computed: {
+    isAuthenticated: {
+      set(isAuthenticated) {
+        return isAuthenticated;
+      },
+      get() {
+        return this[Auth.GET_IS_AUTHENTICATED]();
+      },
+    },
+  },
+  async created() {
     this[Auth.CHECK_IS_AUTHENTICATED]();
+    this.isAuthenticated = this[Auth.GET_IS_AUTHENTICATED]();
 
-    if (this[Auth.GET_IS_AUTHENTICATED]()) {
+    if (this.isAuthenticated) {
       logger.info('Entry/created: is authenticated');
-      this[Auth.DECODE_ID_TOKEN]();
+      await this[Auth.DECODE_ID_TOKEN]();
       // stay at route, otherwise we would redirect the user
       // to the overview page after every reload
     } else {
@@ -39,4 +55,16 @@ export default Vue.extend({
 <style lang="scss">
 @import '~bootstrap/scss/bootstrap';
 @import '~bootstrap-vue/src/index.scss';
+
+body {
+  font-family: 'Hind Vadodara', sans-serif;
+}
+
+.medium {
+  font-weight: 500;
+}
+
+.semibold {
+  font-weight: 600;
+}
 </style>
